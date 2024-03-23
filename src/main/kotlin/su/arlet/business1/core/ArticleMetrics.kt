1a11ac3beaa696e494989576fb00b650f0a249e5
arlet <shulga@niuitmo.ru>
2024-03-23 17:28:26 +0300

    fix metrics

--- src/main/kotlin/su/arlet/business1/core/ArticleMetrics.kt
+++ src/main/kotlin/su/arlet/business1/core/ArticleMetrics.kt
@@ -1,13 +1,12 @@
 package su.arlet.business1.core
 
-import jakarta.persistence.Entity
-import jakarta.persistence.Id
-import jakarta.persistence.Table
+import jakarta.persistence.*
 
 @Entity
 @Table(name = "article_metrics")
 class ArticleMetrics(
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long = 0,
     var viewCounter: Int = 0,
     var readCounter: Int = 0,
